4d8dc680baf3723438cc17ccf35d62706a1efb63
SyedJawadAhmed <jawadezzra06@gmail.com>
2025-08-19 21:18:41 +0500

    😍

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 var swiper = new Swiper(".mySwiper", {
-    loop: true,
+    // loop: true,
     // autoplay: {
     //     delay: 3000,
     //     disableOnInteraction: false,
     // },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: "#Next",
+        prevEl: "#Prev",
     },
 });
