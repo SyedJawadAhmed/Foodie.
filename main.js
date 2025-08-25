bf8a91d7a689d07d67f60dd95c7751c3b95af163
SyedJawadAhmed <jawadezzra06@gmail.com>
2025-08-25 22:32:14 +0500

    🥮

--- main.js
+++ main.js
@@ -15,23 +15,33 @@ const cartIcon = document.querySelector(".cart-icon");
 const cartTab = document.querySelector(".cart-tab");
 const closeBtn = document.querySelector(".Close-btn");
 
-cartIcon.addEventListener("click",()=> cartTab.classList.add('cart-tab-active'));
-closeBtn.addEventListener("click",()=> cartTab.classList.remove('cart-tab-active'));
+cartIcon.addEventListener("click", () => cartTab.classList.add('cart-tab-active'));
+closeBtn.addEventListener("click", () => cartTab.classList.remove('cart-tab-active'));
 
 let productList = [];
 
-const showCard = () =>{
+const showCard = () => {
 
-    productList.forEach(product =>{
+    productList.forEach(product => {
 
         const orderCard = document.createElement('div');
         orderCard.classList.add('order-card');
+
+        orderCard.innerHTML = `
+        
+            <div class="card-image">
+                <img src="images/burger.png" alt="">
+            </div>
+            <h4>Doubble Beef Burger</h4>
+            <h4 class="price">$200</h4>
+            <a href="#" class="btn">Add To Cart</a>
+        `;
     })
 }
 
-const initApp = () =>{
+const initApp = () => {
 
-    fetch('products.json').then(response => response.json()).then(data =>{
+    fetch('products.json').then(response => response.json()).then(data => {
         productList = data;
         console.log(productList);
     })
